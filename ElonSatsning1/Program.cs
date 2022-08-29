79a539bc9332f51c4fcbca32b1b63c839a02a4d1
Laroub03 <Ayoublaroub@gmail.com>
2022-08-29 20:39:34 +0200

    ElonSatsning

---
+++ ElonSatsning1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace ElonSatsning
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+            Console.WriteLine("The battery is at {0}%", car.Start());
+            car.Color();
+            Console.WriteLine("The color of the RC car is {0}", car._Color);
+            while (car._Battery != 0)
+            {
+                Console.WriteLine("Enter the amount of meters the RC car have driven");
+                car.Drive();
+                Console.WriteLine("There is now {0}% battery left\n", car._Battery);
+                if (car._Battery == 0)
+                {
+                    Console.WriteLine("The RC car ran out of battery. Please recharge!");
+                }
+            }
+
+        }
+    }
+}
---
+++ ElonSatsning1/car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElonSatsning
+{
+    public class Car
+        {
+            private int _battery;
+            private int _meter;
+            private string _color;
+
+
+            public int _Battery
+            { get { return _battery; } set { _battery = value; } }
+            public int _Meter
+            { get { return _meter; } set { _meter = value; } }
+            public string _Color
+            { get { return _color; } set { _color = value; } }
+
+
+            public int Start()
+            {
+                _Battery = 100;
+                return _Battery;
+            }
+            public void Color()
+            {
+                string[] color = { "Red", "Blue", "Green", "Yellow" };
+                Random random = new Random();
+                int randomIndex = random.Next(1, 4);
+                string selectedColor = color[randomIndex];
+                _color = selectedColor;
+            }
+
+            public void Drive()
+            {
+                int _Meter = int.Parse(Console.ReadLine());
+                if (_Battery > 0)
+                {
+                    _Meter /= 20;
+                    _battery -= _Meter;
+                }
+       }
+   }
+}
