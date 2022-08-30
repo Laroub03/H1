286d856726763bc6f33d5c609a6d22585359840f
Laroub03 <Ayoublaroub@gmail.com>
2022-08-30 20:57:38 +0200

    RobotsComing

---
+++ RobotsComing/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsComing
+{
+    public enum Microchips
+    {
+        RX54667,
+        QT8339, 
+    }
+ 
+    internal class Robot
+    {
+        private string _color;
+        private int _battery;
+        private int _soap;
+        private int _wheels;
+        private Microchips _microchips;
+        public string _Color
+        { get => _color; }             
+
+        public int _Battery
+            { get => _battery; }
+
+        public int _Soap
+            { get => _soap; }
+
+        public int _Wheels
+            { get => _wheels; }
+        public Microchips _Microchips
+            { get => _microchips; }
+        public void Color()
+        {
+            string[] color = { "White", "Blue", "Green" };
+            Random random = new Random();
+            int randomIndex = random.Next(0, 3);
+            string selectedColor = color[randomIndex];
+            _color = selectedColor;
+        }
+
+        public void Battery()
+        {
+            if (_color != "White")
+            {
+                Random random = new Random();
+                int battery = random.Next(0, 255);
+                _battery = battery;
+            }
+        }
+
+        public void Wheels()
+        {
+            int wheels = int.Parse(Console.ReadLine());
+            if (wheels == 0)
+            {
+                Console.WriteLine("The size of the robot is small");
+            }
+            else if (wheels > 0)
+            {
+                Console.WriteLine("The size of the robot is medium");
+            }
+            else if (wheels > 5)
+            {
+                Console.WriteLine("The size of the robot is large");
+            }
+        }
+
+        public Robot()
+        {
+            _soap = _soap;
+            _soap = 2300;
+            Random random = new Random();
+            Microchips mc = (Microchips)random.Next(2);
+            _microchips = mc;
+            Color();
+            Battery();
+        }
+
+
+    }
+}
---
+++ RobotsComing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Globalization;
+
+namespace RobotsComing
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Robot robot = new Robot();
+            string[] WIFI = { "The Robot does not have WIFI", "The Robot does have WIFI", };
+            Random random = new Random();
+            int randomIndex = random.Next(2);
+            string selectedWifi = WIFI[randomIndex];
+
+            Console.WriteLine(selectedWifi);
+            Console.WriteLine("The model name of the microchip is {0}", robot._Microchips);
+            Console.WriteLine("The color of the robot is {0}", robot._Color);
+            Random random1 = new Random();
+            int soap = random1.Next(0,2);
+            if (soap == 1)
+            {
+                Console.WriteLine("The robot contains {0}ml soap", robot._Soap);
+            }
+            else if (robot._Color == "White")
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("The capacity of the battery is {0}", robot._Battery);
+            }
+            Console.WriteLine("\nEnter the amount of wheels the robot has");
+            robot.Wheels();
+        }
+    }
+}
