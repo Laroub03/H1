e8f2fbc21bf752c0285634bcb37a360532b523ba
Laroub03 <Ayoublaroub@gmail.com>
2022-09-01 08:18:40 +0200

    SortingHat

---
+++ SortingHat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Globalization;
+
+namespace SortingHat
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            SortingHat sortingHat = new SortingHat();
+            Console.WriteLine("Mmmm... differcult. Very differcult");
+            Thread.Sleep(2000);
+            Console.WriteLine("Where to put you?");
+            Thread.Sleep(2000);
+            Console.WriteLine("Ha now I know just what to do with you!");
+            Thread.Sleep(2000);
+            Console.WriteLine("{0}!", sortingHat._Groups);
+        }
+    }
+}
---
+++ SortingHat/SortingHat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingHat
+{
+    public enum Groups
+    {
+        Gryffindor,
+        Hufflepuff,
+        Ravenclaw,
+        Slythetin
+    }
+    internal class SortingHat
+    {
+        private Groups _groups;
+
+        public Groups _Groups
+        { get => _groups; }
+
+        public SortingHat()
+        {
+            Random random = new Random();
+            Groups groups = (Groups)random.Next(0, 4);
+            _groups = groups;
+        }
+    }
+}
