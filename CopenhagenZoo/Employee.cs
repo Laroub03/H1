68755198e06bef9eec538dfb7154d7c46271561d
Laroub03 <Ayoublaroub@gmail.com>
2022-09-05 23:20:48 +0200

    Copenhagen Zoo

---
+++ CopenhagenZoo/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeper
+{
+    
+    internal class Employee
+    {
+        private int id;
+        private string name;
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+
+        public void EmployeeID()
+        {
+            Random rnd = new Random();
+
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(rnd.Next(5));
+            }
+        }
+    }
+}
